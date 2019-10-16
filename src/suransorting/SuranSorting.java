d9be7b466eb7fa916b05c3018e6b5b50f4b8272c
Chase Dawson <EKUStudent@HPMXL4271QDP.student.eku.edu>
2019-10-16 15:52:41 -0400

    Cleaned up code, erased unnecessary comments.

--- src/suransorting/SuranSorting.java
+++ src/suransorting/SuranSorting.java
@@ -6,13 +6,13 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Chase Dawson
  */
 public class SuranSorting {
 
-    /* This method sorts the text file stored in test\names.txt
-       by length and then by alphabet
+    /* This application takes the strings contained in test/names.txt and adds 
+       them to an arraylist while dynamically sorting them by length, then by
+       alphabet
      */
     public static void main(String[] args) {
         File file = new File("test\\names.txt");
@@ -33,37 +33,33 @@ public class SuranSorting {
                 // Verify the string is at least length 1, excluding white space
                 // If valid, sort the values dynamically while adding them
                 int index = 0;
-                if (newString.length() > 0) // Sort them by length
-                {
+                if (newString.length() > 0) {
+                    // Sort them by length
                     for (int i = 0; i < names.size(); i++) {
                         if (newString.length() > names.get(i).length()) {
                             index++;
-                        }
-                        else
+                        } else {
                             break;
+                        }
                     }
                     // Sort them alphabetically
-                    for (int i = index+1; i < names.size(); i++) {
+                    for (int i = index + 1; i < names.size(); i++) {
                         if (newString.length() < names.get(i).length()) {
                             break;
                         }
-                        
+
                         String compareString = names.get(i);
                         int charIndex = 0;
-                        
+
                         while (newString.charAt(charIndex) == compareString.charAt(charIndex)) {
                             charIndex++;
                         }
                         if (newString.charAt(charIndex) > compareString.charAt(charIndex)) {
                             index++;
-//                            newString = names.get(i);
-//                            names.set(i, names.get(j));
-//                            names.set(j, newString);
                         }
                     }
-                    names.add(index,newString);
+                    names.add(index, newString);
                 }
-
             }
         } catch (IOException ex) {
             Logger.getLogger(SuranSorting.class.getName()).log(Level.SEVERE, null, ex);
@@ -73,7 +69,6 @@ public class SuranSorting {
         for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
         }
-
     }
 
 }
